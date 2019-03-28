5d97622118b3c2f4b2f9222e7eabcbeb5210b3de
privateryan1111 <ryanpfeifer1111@gmail.com>
2019-03-28 11:43:09 -0500

    added a few more tweaks

--- Assets/OverlayDemo/Scripts/KinectOverlayer.cs
+++ Assets/OverlayDemo/Scripts/KinectOverlayer.cs
@@ -3,19 +3,18 @@ using System.Collections;
 
 public class KinectOverlayer : MonoBehaviour 
 {
-//	public Vector3 TopLeft;
-//	public Vector3 TopRight;
-//	public Vector3 BottomRight;
-//	public Vector3 BottomLeft;
+    public float y_rotation_intensity = 3.0f;
+    public float x_rotation_intensity = 1.0f;
 
-	public GUITexture backgroundImage;
+
+    public GUITexture backgroundImage;
 	public KinectWrapper.NuiSkeletonPositionIndex TrackedJoint = KinectWrapper.NuiSkeletonPositionIndex.HandRight;
 	public GameObject OverlayObject;
-	public float smoothFactor = 5f;
+    public float smoothFactor = 25f;//5f;
 	
 	public GUIText debugText;
 
-    //public GameObject cam;
+    public Camera cam;
 
     //public Transform camPos;
 
@@ -79,8 +78,9 @@ public class KinectOverlayer : MonoBehaviour
 						
 						if(OverlayObject)
 						{
-							Vector3 vPosOverlay = Camera.main.ViewportToWorldPoint(new Vector3(scaleX, scaleY, distanceToCamera));
+                            Vector3 vPosOverlay = Camera.main.ViewportToWorldPoint(new Vector3(scaleX, scaleY, distanceToCamera));
                             OverlayObject.transform.position = Vector3.Lerp(OverlayObject.transform.position, vPosOverlay, smoothFactor * Time.deltaTime);
+                            //COPYLOCATION ? <<- lookup
                             //Vector3 ballPos = new Vector3();
                             //ballPos.Set(OverlayObject.transform.position.x, OverlayObject.transform.position.y, OverlayObject.transform.position.z);
                             float y = OverlayObject.transform.position.y;
@@ -89,7 +89,6 @@ public class KinectOverlayer : MonoBehaviour
                             {
                                 y = 1.0f;
                             }
-                            /*
                             
                             if(x < -5.0f)
                             {
@@ -99,12 +98,21 @@ public class KinectOverlayer : MonoBehaviour
                             {
                                 x = 5.0f;
                             }
-                            */
+                            
                             OverlayObject.transform.position = new Vector3(x, y, OverlayObject.transform.position.z);
-                            OverlayObject.transform.rotation = Quaternion.Euler(new Vector3(OverlayObject.transform.position.y * 3.0f, OverlayObject.transform.position.x * -3.0f, 0));
+                            
+                            OverlayObject.transform.rotation = Quaternion.Euler(new Vector3(OverlayObject.transform.position.y * y_rotation_intensity, -OverlayObject.transform.position.x * x_rotation_intensity, 0));
                             //OverlayObject.transform.rotation = Quaternion.Euler(new Vector3(ballPos.y * 3.0f, ballPos.x * -3.0f, 0));
 
+                            /*
                             //camPos.position = Vector3.Lerp(OverlayObject.transform.position, vPosOverlay, smoothFactor * Time.deltaTime);
+                            left = -OverlayObject.transform.position.x;
+                            right = OverlayObject.transform.position.x;
+                            bottom = -OverlayObject.transform.position.y;
+                            top = OverlayObject.transform.position.y;
+                            Matrix4x4 m = PerspectiveOffCenter(left, right, bottom, top, cam.nearClipPlane, cam.farClipPlane);
+                            cam.projectionMatrix = m;
+                            */
                         }
                     }
 				}
@@ -113,4 +121,33 @@ public class KinectOverlayer : MonoBehaviour
 			
 		}
 	}
+    static Matrix4x4 PerspectiveOffCenter(float left, float right, float bottom, float top, float near, float far)
+    {
+        float x = 2.0F * near / (right - left);
+        float y = 2.0F * near / (top - bottom);
+        float a = (right + left) / (right - left);
+        float b = (top + bottom) / (top - bottom);
+        float c = -(far + near) / (far - near);
+        float d = -(2.0F * far * near) / (far - near);
+        float e = -1.0F;
+        Matrix4x4 m = new Matrix4x4();
+        m[0, 0] = x;
+        m[0, 1] = 0;
+        m[0, 2] = a;
+        m[0, 3] = 0;
+        m[1, 0] = 0;
+        m[1, 1] = y;
+        m[1, 2] = b;
+        m[1, 3] = 0;
+        m[2, 0] = 0;
+        m[2, 1] = 0;
+        m[2, 2] = c;
+        m[2, 3] = d;
+        m[3, 0] = 0;
+        m[3, 1] = 0;
+        m[3, 2] = e;
+        m[3, 3] = 0;
+        return m;
+    }
+
 }
